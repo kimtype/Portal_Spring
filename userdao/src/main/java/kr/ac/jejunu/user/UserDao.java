5552a1fba0567ad27004c8703dcf910679216ffe
kimtype <kimtybe@naver.com>
2020-04-25 00:18:25 +0900

    0424 (4)-4

--- userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -16,7 +16,7 @@ public class UserDao {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
-        User user;
+        User user = null;
         try {
             connection = dataSource.getConnection();
 
@@ -24,13 +24,13 @@ public class UserDao {
             preparedStatement.setInt(1, id);
 
             resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-
+            if(resultSet.next()){
             user = new User();
-
             user.setId(resultSet.getInt("id"));
             user.setName(resultSet.getString("name"));
             user.setPassword(resultSet.getString("password"));
+            }
+
         } finally {
             try {
                 resultSet.close();
@@ -49,8 +49,6 @@ public class UserDao {
             }
 
         }
-
-
         return user;
     }
 
@@ -92,4 +90,54 @@ public class UserDao {
 
     }
 
+    public void update(User user) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "update  userinfo set name = ?, password = ?where id = ?");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, user.getId());
+            preparedStatement.executeUpdate();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void delete(Integer id) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "delete from userinfo where id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- userdao/src/test/java/kr/ac/jejunu/user/userDaoTests.java
+++ userdao/src/test/java/kr/ac/jejunu/user/userDaoTests.java
@@ -1,6 +1,7 @@
 package kr.ac.jejunu.user;
 
 
+import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -39,41 +40,46 @@ public class userDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-
         userDao.insert(user);
         assertThat(user.getId(), greaterThan(0));
 
         User insertedUser = userDao.get(user.getId());
         assertThat(insertedUser.getName(),is(name));
         assertThat(insertedUser.getPassword(), is(password));
-    }/*
-    @Test
-    public void getHalla() throws SQLException, ClassNotFoundException {
-        Integer id = 1;
-
-
-        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        User user = userDao.get(id);
-        assertThat(user.getId(), is(id));
-        assertThat(user.getName(), is(name));
-        assertThat(user.getPassword(), is(password));
     }
     @Test
-    public void insertHalla() throws SQLException, ClassNotFoundException {
+    public void update() throws SQLException {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
         userDao.insert(user);
-        assertThat(user.getId(), greaterThan(0));
 
-        User insertedUser = userDao.get(user.getId());
-        assertThat(insertedUser.getName(),is(name));
-        assertThat(insertedUser.getPassword(), is(password));
+        String updatedName = "type";
+        user.setName(updatedName);
+        String updatedPassword = "1111";
+        user.setPassword(updatedPassword);
+
+        userDao.update(user);
+
+        User updatedUser = userDao.get(user.getId());
+        assertThat(updatedUser.getName(), is(updatedName));
+        assertThat(updatedUser.getPassword(), is(updatedPassword));
     }
-*/
+
+    @Test
+    public void delete() throws SQLException {
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDao.insert(user);
+
+        userDao.delete(user.getId());
+
+        User deletedUser = userDao.get(user.getId());
+
+        assertThat(deletedUser, IsNull.nullValue());
+
+    }
 
 }
 
